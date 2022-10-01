64970e950e85e84b4869dfa0d2461b3ed7ca9e5e
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-10-01 17:28:31 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿ namespace MaximumSubarray
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+            int result = MaxSubArray(nums);
+            Console.WriteLine(result);
+        }
+
+        public static int MaxSubArray(int[] nums) {
+            int size = nums.Length;
+            int max = 0,
+                max_ending = 0;
+            
+            for (int i =0; i < size; i++)
+            {
+                max_ending = max_ending + nums[i];
+                if(max < max_ending)
+                    max = max_ending;
+                if(max_ending < 0)
+                    max_ending = 0;
+            }
+            return max;
+        }
+    }
+ }
